96e15011a78ffa029f8caebccf56d651e000c553
ezdes <71791856+ezdes@users.noreply.github.com>
2021-02-25 20:15:45 +0600

    some updates

--- taskB_exam4/src/DbConnection.java
+++ taskB_exam4/src/DbConnection.java
@@ -3,14 +3,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
-    private static final String url = "jdbc:postgresql://localhost:5432/exam4";
-    private static final String user = "postgres";
-    private static final String password = "2m3410lt";
+    private static final String URL = "jdbc:postgresql://localhost:5432/exam4";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "2m3410lt";
 
     public static Connection connect() {
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection(url, user, password);
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
         }
 
         catch (SQLException e) {
